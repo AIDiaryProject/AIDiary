4e613a7f5c33adf3f0e5484a96b21524cbd6ff51
ljh00112233 <l268728@naver.com>
2025-04-17 15:04:33 +0900

    0414_Lee_updateDB

--- others/diaryDB.js
+++ others/diaryDB.js
@@ -22,7 +22,6 @@ router.post('/diarysave', async (req, res) => {
   });
 
 // 일기 데이터 조회
-{/* 
 router.get('/', async (req, res) => {
   try {
       const [rows] = await db.execute('SELECT * FROM diaryDB');
@@ -30,18 +29,6 @@ router.get('/', async (req, res) => {
   } catch (err) {
       console.error("DB 조회 중 에러 발생:", err); // 에러 로그 추가
       res.status(500).json({ error: 'DB 조회 실패' });
-  }
-});
-*/}
-
-router.get('/', authMiddleware, async (req, res) => {
-  try {
-    const userId = req.user.id; // 토큰에서 추출한 사용자 ID
-    const [rows] = await db.execute('SELECT * FROM diaryDB WHERE user_id = ?', [userId]);
-    res.json(rows);
-  } catch (err) {
-    console.error("DB 조회 중 에러 발생:", err);
-    res.status(500).json({ error: 'DB 조회 실패' });
   }
 });
 
