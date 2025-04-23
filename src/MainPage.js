ac78301f679d3eadb9b46122db8141cf0bf98782
Hyeongun513 <ph00513@naver.com>
2025-04-23 15:03:39 +0900

    0423_Park_CssUpdate

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import LoginUser from './Park/LoginUser';
+import './App.scss'
 
 const MainPage = () => {
     const navigate = useNavigate();
